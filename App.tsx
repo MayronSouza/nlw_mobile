6c87af4bb4737dbef2e4082f21495c1fb92c35bd
MayronSouza <mayron.souza@gmz.solutions>
2020-08-06 18:09:16 -0300

    Estilizando e estruturando a Landing Page

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
+
+import Landing from './src/pages/Landing/index'
+
+const App = () => {
+  return (
+    <>
+      <Landing />
+    </>
+  )
+};
+
+export default App;
